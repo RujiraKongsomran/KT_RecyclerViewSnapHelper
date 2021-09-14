1efccc24bb96c6b10b5a4708e2edd75a66abbf1f
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-09-14 19:26:55 +0700

    Dummy Data

--- app/src/main/java/com/rujirakongsomran/kt_recyclerviewsnaphelper/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/kt_recyclerviewsnaphelper/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.rujirakongsomran.kt_recyclerviewsnaphelper
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.rujirakongsomran.kt_recyclerviewsnaphelper.adapter.ExampleAdapter
+import com.rujirakongsomran.kt_recyclerviewsnaphelper.databinding.ActivityMainBinding
+import com.rujirakongsomran.kt_recyclerviewsnaphelper.model.ExampleItem
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val exampleList = generateDummyList(500)
+        binding.recyclerView.adapter = ExampleAdapter(exampleList)
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.setHasFixedSize(true)
+
+    }
+
+    private fun generateDummyList(size: Int): List<ExampleItem> {
+        val list = ArrayList<ExampleItem>()
+
+        for (i in 0 until size) {
+            val drawable = when (i % 3) {
+                0 -> R.drawable.ic_android
+                1 -> R.drawable.ic_audio
+                else -> R.drawable.ic_sun
+            }
+
+            val item = ExampleItem(drawable, "Item $i", "Line 2")
+            list += item
+        }
+        return list
     }
 }
